a3638ad548bcd2c7cbb37f90355422b5dfb8968a
Tathn <siewniak.patryk@gmail.com>
2024-12-07 19:00:52 +0100

    day5 fin

--- 2024/day5/main.go
+++ 2024/day5/main.go
@@ -15,8 +15,8 @@ var input string
 //go:embed input_example.txt
 var inputExample string
 
-func parseRules(rules []string) map[int]int {
-    parsedRules := make(map[int]int)
+func parseRules(rules []string) map[int][]int {
+    parsedRules := make(map[int][]int)
     for _, rule := range rules {
         x, y, found := strings.Cut(rule, "|")
         if !found {
@@ -31,14 +31,95 @@ func parseRules(rules []string) map[int]int {
             panic(fmt.Sprintf("Could not convert rules to int, x=%s, y=%s", x, y))
         }
 
-        parsedRules[yInt] = xInt
+        parsedRules[yInt] = append(parsedRules[yInt], xInt)
     }
     return parsedRules
 }
 
+func commaSeparatedStringToIntSlice(str string) []int {
+    splitted := strings.Split(str, ",")
+    slice := make([]int, len(splitted))
+    for index, entry := range splitted {
+        asInt, err := strconv.Atoi(entry)
+        if err != nil {
+            panic(err)
+        }
+
+        slice[index] = asInt
+    }
+    return slice
+}
+
+func splitUpdatedRulesOrder(updates []string, rules map[int][]int) ([][]int,[][]int) {
+    correctlyOrderedUpdates := make([][]int, 0)
+    incorrectlyOrderedUpdates := make([][]int, 0)
+    for _, update := range updates {
+        pages := commaSeparatedStringToIntSlice(update)
+
+        isUpdateCorrectlyOrdered := true
+        for pageIndex, page := range pages {
+            if ruleEntries, exists := rules[page]; exists {
+                for _, entry := range ruleEntries {
+                    if entryIndex := slices.Index(pages, entry); entryIndex != -1 {
+                        if entryIndex > pageIndex {
+                            isUpdateCorrectlyOrdered = false
+                            break
+                        }
+                    }
+                }
+            }
+            if !isUpdateCorrectlyOrdered {
+                break
+            }
+        }
+        if isUpdateCorrectlyOrdered {
+            correctlyOrderedUpdates = append(correctlyOrderedUpdates, pages)
+            continue
+        } else {
+            incorrectlyOrderedUpdates = append(incorrectlyOrderedUpdates, pages)
+        }
+    }
+
+    return correctlyOrderedUpdates, incorrectlyOrderedUpdates
+}
+
+func fixPagesOrder(updates [][]int, rules map[int][]int) [][]int {
+    fixedUpdates := make([][]int, 0)
+    for _, update := range updates {
+        isUpdateFixed := false
+        for !isUpdateFixed {
+ //           updateCpy := make([]int, len(update))
+            for pageIndex, page := range update {
+                isPageFixed := true
+                if ruleEntries, exists := rules[page]; exists {
+                    for _, entry := range ruleEntries {
+                        if entryIndex := slices.Index(update, entry); entryIndex != -1 {
+                            if entryIndex > pageIndex {
+                                log.Println("Swapping",update[entryIndex], "with", update[pageIndex], "in", update)
+                                tmp := update[entryIndex]
+                                update[entryIndex] = update[pageIndex]
+                                update[pageIndex] = tmp
+                                log.Println("Result", update)
+                                isPageFixed = false
+                                isUpdateFixed = false
+                                break
+                            }
+                        }
+                    }
+                }
+                isUpdateFixed = isPageFixed
+                if !isUpdateFixed {
+                    break
+                }
+            }
+        }
+        fixedUpdates = append(fixedUpdates, update)
+    }
+    return fixedUpdates
+}
+
 func Main() {
-    //TODO rules parsed wrong
-    lines := strings.Split(inputExample, "\n")
+    lines := strings.Split(input, "\n")
     lines = lines[:len(lines)-1]
     partOneResult := 0
     partTwoResult := 0
@@ -52,41 +133,22 @@ func Main() {
     log.Println(updates)
 
     rules := parseRules(pageOrderingRules)
-    correctlyOrderedUpdates := make([][]int, 0)
-    for _, update := range updates {
-        ps := strings.Split(update, ",")
-        pages := make([]int, len(ps))
-        for index, page := range ps {
-            p, err := strconv.Atoi(page)
-            if err != nil {
-                panic(err)
-            }
+    correctlyOrderedUpdates, incorrectlyOrderedUpdates := splitUpdatedRulesOrder(updates, rules)
+    log.Println("rules:", rules)
+    log.Println("Correct:",correctlyOrderedUpdates)
+    log.Println("Incorrect:", incorrectlyOrderedUpdates)
 
-            pages[index] = p
-        }
-
-        isUpdateCorrectlyOrdered := true
-        for pageIndex, page := range pages {
-            if requiredPage, exists := rules[page]; exists {
-                if requiredPageIndex := slices.Index(pages, requiredPage); requiredPageIndex != -1 {
-                    if requiredPageIndex > pageIndex {
-                        isUpdateCorrectlyOrdered = false
-                    }
-                }
-            }
-        }
-        if isUpdateCorrectlyOrdered {
-            correctlyOrderedUpdates = append(correctlyOrderedUpdates, pages)
-        }
-    }
-    log.Println(rules)
-    log.Println(correctlyOrderedUpdates)
-
-    sum := 0
+    partOneResult = 0
     for _, update := range correctlyOrderedUpdates {
         mid := update[len(update)/2]
-        sum += mid
+        partOneResult += mid
     }
-    log.Println(sum)
+    fixedUpdates := fixPagesOrder(incorrectlyOrderedUpdates, rules)
+    log.Println("Fixed:", fixedUpdates)
+    partTwoResult = 0
+    for _, update := range fixedUpdates {
+        mid := update[len(update)/2]
+        partTwoResult += mid
+    }
     log.Println("=====================\nPart One:", partOneResult, "\nPart Two:", partTwoResult)
 }
